2362d0027767808b88de75d48e212832d892f29d
Asanka Munasinghe <asankalk@gmail.com>
2016-09-07 19:11:29 +0800

    Initial commit and layout

---
+++ app/src/main/java/au/edu/murdoch/ict376/tictactoe/MainActivity.java
@@ -0,0 +1,13 @@
+package au.edu.murdoch.ict376.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
